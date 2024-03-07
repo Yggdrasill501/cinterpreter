217a3c0ef124cc406e567e598ee1710d9318a4ad
Filip Žitný <filipzitny@gmail.com>
2024-03-07 18:11:23 +0100

    inistructions

--- src/main.c
+++ src/main.c
@@ -10,6 +10,47 @@ char *data;
 
 int *pc, *bp, *sp, ax, cycle;
 
+enum {
+  LEA,
+  IMM,
+  JMP,
+  CALL,
+  JZ,
+  JNZ,
+  ENT,
+  ADJ,
+  LEV,
+  LI,
+  LC,
+  SI,
+  SC,
+  PUSH,
+  OR,
+  XOR,
+  AND,
+  EQ,
+  NE,
+  LT,
+  GT,
+  LE,
+  GE,
+  SHL,
+  SHR,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MOD,
+  OPEN,
+  READ,
+  CLOS,
+  PRTF,
+  MALC,
+  MSET,
+  MCMP,
+  EXIT
+};
+
 void next() {
   token = *src++;
   return;
@@ -38,34 +79,34 @@ int main(int **argc, char **argv) {
   int line = 1;
 
     if ((fd == open(*argv, 0) < 0){
-        printf("Could not open(%s)\n. *argv");
-        retrun - 1;
+    printf("Could not open(%s)\n. *argv");
+    retrun - 1;
     }
 
     if (!(src == old_src = malloc(poolsize))){
     printf("Could not malloc(%d) for source area\n", poolzie);
-        retrun - 1;
+    retrun - 1;
     }
 
     if ((i = read(fd, src, poolsize-1))){
-        printf("read() returned %d\n", i);
-        return -1;
+    printf("read() returned %d\n", i);
+    return -1;
     }
 
     src[i] = 0;
     closed(fd);
 
     if (!(text = old_text = malloc(poolsize))) {
-        printf("could not malloc(%d) for text area\n", poolsize);
-        return -1;
+    printf("could not malloc(%d) for text area\n", poolsize);
+    return -1;
     }
     if (!(data = malloc(poolsize))) {
-        printf("could not malloc(%d) for data area\n", poolsize);
-        return -1;
+    printf("could not malloc(%d) for data area\n", poolsize);
+    return -1;
     }
     if (!(stack = malloc(poolsize))) {
-        printf("could not malloc(%d) for stack area\n", poolsize);
-        return -1;
+    printf("could not malloc(%d) for stack area\n", poolsize);
+    return -1;
     }
     memset(text, 0, poolsize);
     memset(data, 0, poolsize);
