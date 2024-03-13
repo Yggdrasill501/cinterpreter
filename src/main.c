92c2d0da309542fd124201b063b82407a38d4620
Filip Žitný <filipzitny@gmail.com>
2024-03-13 09:35:20 +0100

    implentation of left recursion

--- src/main.c
+++ src/main.c
@@ -370,6 +370,59 @@ int eval() {
   return 0;
 }
 
+int expr();
+
+int factor() {
+    int value = 0;
+    if (token == '(') {
+        match('(');
+        value = expr();
+        match(')');
+    } else {
+        value = token_val;
+        match(Num);
+    }
+    return value;
+}
+
+int term_tail(int lvalue) {
+    if (token == '*') {
+        match('*');
+        int value = lvalue * factor();
+        return term_tail(value);
+    } else if (token == '/') {
+        match('/');
+        int value = lvalue / factor();
+        return term_tail(value);
+    } else {
+        return lvalue;
+    }
+}
+
+int term() {
+    int lvalue = factor();
+    return term_tail(lvalue);
+}
+
+int expr_tail(int lvalue) {
+    if (token == '+') {
+        match('+');
+        int value = lvalue + term();
+        return expr_tail(value);
+    } else if (token == '-') {
+        match('-');
+        int value = lvalue - term();
+        return expr_tail(value);
+    } else {
+        return lvalue;
+    }
+}
+
+int expr() {
+    int lvalue = term();
+    return expr_tail(lvalue);
+}
+
 enum { CHAR, INT, PTR };
 int *idmain;                  // the `main` function
 
